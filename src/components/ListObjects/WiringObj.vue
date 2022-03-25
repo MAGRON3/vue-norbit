7ebc36b5d0382580a7f194f1fcae58767e4eeece
MAGRON3 <48138125+MAGRON3@users.noreply.github.com>
2022-03-25 14:29:17 +0300

    master

---
+++ src/components/ListObjects/WiringObj.vue
@@ -0,0 +1,76 @@
+<template>
+    <tr>
+        <td>{{index + 1}}</td>
+        <td>{{GetTaskName}}</td>
+        <td>{{c_wiring.w_date}}</td>
+        <td>{{c_wiring.w_hours}}</td>
+        <td>{{c_wiring.name}}</td>
+        <!-- <td>
+            <div>
+                <button class="btn_ed" @click="ChangeWiring">E</button>
+                <button class="btn_rm" @click="DeleteWiring">&times;</button>
+            </div>
+        </td> -->
+    </tr>
+</template>
+
+<script>
+export default {
+    props: {
+        c_wiring:
+        {
+            type: Object,
+            required: true
+        },
+        current_tasks:
+        {
+            type: Object,
+            required: true
+        },
+        index: Number,
+    },
+
+    methods: {
+
+    },
+
+    computed:{
+        GetTaskName(){
+            for(let i = 0; i < this.current_tasks.length; i++){
+                if (this.current_tasks.at(i).id === this.c_wiring.taskID) 
+                    return this.current_tasks.at(i).name;
+            }
+            return '';
+        }
+    },
+
+    data(){
+        return{
+
+        }
+    },
+}
+</script>
+
+<style scoped>
+.btn_rm {
+    background: red;
+    color: #fff;
+    border-radius: 50%;
+    font-weight: bold;
+    margin: 2px;
+}
+
+.btn_ed {
+    background: green;
+    color: #fff;
+    border-radius: 50%;
+    font-weight: bold;
+    margin: 2px;
+}
+
+.input_edit{
+    width: 95%;
+    text-align: center;
+}
+</style>
---
+++ src/components/Lists/WiringList.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="frame_div">
+        <div v-if="current_wirings.length">
+            <strong>List</strong>
+            <table width="500px">
+                <tr>
+                    <td width="5%">№</td>
+                    <td width="30%">Task Name</td>
+                    <td width="20%">Date</td>
+                    <td width="5%">Hours</td>
+                    <td width="50%">Description</td>
+                    <!-- <td width="15%"></td> -->
+                </tr>
+                <WiringObj
+                    v-for="(c_wiring,idx) in current_wirings"
+                    v-bind:key="c_wiring.id"
+                    v-bind:index="idx"
+                    v-bind:c_wiring="c_wiring"
+                    v-bind:current_tasks="current_tasks"
+                    @delete-wiring="DeleteWiring"
+                    @change-wiring="ChangeWiring"
+                />
+            </table>
+        </div>
+        <div v-else>
+            <strong>Not wirings</strong>
+        </div>
+    </div>
+</template>
+
+<script>
+import WiringObj from '@/components/ListObjects/WiringObj.vue'
+export default {
+    props: {
+        current_wirings:
+        {
+            type: Object,
+            required: true
+        },
+        current_tasks:
+        {
+            type: Object,
+            required: true
+        },
+    },
+
+    components: { WiringObj },
+
+    data() {
+        return{
+
+        }
+    },
+    
+    methods:{
+
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
